556cbe5abf0609b205d9a65010e26c1b15fcc10e
LalitBiswal123 <Lalit_Biswal@Dell.com>
2022-03-18 14:47:33 +0530

    DsaTestCaseBase changes

--- DSA_TESTS/TestCases/DsaTestCaseBase.cs
+++ DSA_TESTS/TestCases/DsaTestCaseBase.cs
@@ -1,89 +1,21 @@
 using System;
-using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Configuration;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Remote;
-using Dell.Adept.UI.Web.Testing.Framework.WebDriver;
 using Dsa.Utils;
-using System.Data;
 using System.IO;
-using System.Net;
-using System.Reflection;
 using Dell.Adept.UI.Web.Testing.Framework;
-using Microsoft.Edge.SeleniumTools;
-using OpenQA.Selenium.Chrome;
 using System.Text;
-using OpenQA.Selenium.Edge;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.IE;
-using OpenQA.Selenium.Safari;
-using OpenQA.Selenium.Support.UI;
 
 namespace DsaTest.TestCases
 {
     [TestClass]
     public class DsaTestCaseBase : WebUIMsTestBase
     {
-        //public static string runSettingsInputXMLFolderPath = "";
         public DsaTestCaseBase()
         {
-            BeforeRemoteWebDriverInitialized += DsaTestCaseBase_BeforeRemoteWebDriverInitialized;
+
         }
-
-        /// <summary>
-        /// Timeout
-        /// </summary>
-        public TimeSpan? WebDriverTimeout { get; set; }
-
-        void DsaTestCaseBase_BeforeRemoteWebDriverInitialized(object sender, RemoteWebDriverEventArgs e)
-        {
-            Logger.Write("Executing tests with RemoteWebDriver...");
-
-            if (!bool.Parse(ConfigurationManager.AppSettings["RunInSauceLabs"]))
-            {
-                dynamic internalGridOptions;
-
-                switch (ConfigurationManager.AppSettings["GridBrowserName"].ToLower())
-                {
-                    case "chrome":
-                        internalGridOptions = new ChromeOptions();
-                        ChromeOptions chromeOptions = new ChromeOptions();
-                        chromeOptions.AddUserProfilePreference("download.default_directory", @"\\W10DTVB9R2\Samuel_G\Sandeep");
-                        internalGridOptions = chromeOptions;
-                        break;
-                    case "internetexplorer":
-                        internalGridOptions = new InternetExplorerOptions();
-                        break;
-                    case "firefox":
-                        internalGridOptions = new FirefoxOptions();
-                        break;
-                    case "edge":
-                        internalGridOptions = new Microsoft.Edge.SeleniumTools.EdgeOptions(); // use the new Microsoft.Edge for new Edge (Chromium) instead of OpenQA.Selenium.Edge
-                        break;
-                    case "legacyedge":
-                        internalGridOptions = new OpenQA.Selenium.Edge.EdgeOptions(); // use OpenQA.Selenium.Edge for legacy edge
-                        break;
-                    default:
-                        internalGridOptions = new ChromeOptions();
-                        break;
-                }
-
-                e.Options.Uri = new Uri(ConfigurationManager.AppSettings["InternalGridHubUrl"]);
-                e.Options.WebDriverOptions = internalGridOptions;
-                WebDriverTimeout = TimeSpan.FromMinutes(10);
-            }
-            else
-            {
-                e.Options.Uri = new Uri(ConfigurationManager.AppSettings["SauceLabsGridHubUrl"]);
-                var sauceLabsDriverOptions = ResolveBrowserDriverOptions(ConfigurationManager.AppSettings["GridBrowserName"]);
-                e.Options.WebDriverOptions = sauceLabsDriverOptions;
-            }
-        }
-
-
-        #region NonTest Code
-
+     
+        #region
 
         [AssemblyInitialize]
         public static void TestAssemblyInitialize(TestContext testContext)
@@ -111,23 +43,30 @@ namespace DsaTest.TestCases
                 }
                 Console.WriteLine("dirpath: " + dirPath);
                 var dirPathValue = dirPath.Remove(dirPath.Length - 1, 1);//to remove diretory path extra slash at end
+
+                //Test Data Source Path - C:\MyDrive\SourceTree\DSA_TESTS\bin\Debug\TestData\US\SIT2
                 string testDatasourceDirPath = dirPathValue + testContext.Properties["LocalBuild"].ToString() + testContext.Properties["LocalBuildBinReleasePath"].ToString() + testContext.Properties["LocalTestDataPath"].ToString();
+
+                //Test Deployment Path - C:\MyDrive\SourceTree\TestResults\Deploy_Lalit_Biswal 2022-03-18 13_52_03\Out\TestData
                 string testDeploymentDirPath = testContext.DeploymentDirectory.ToString() + "\\TestData";
+
+                //C:\MyDrive\SourceTree\DSA_TESTS\bin\Debug\TestData\US\SIT2
                 DirectoryInfo sourceDir = new DirectoryInfo(testDatasourceDirPath);
                 Console.WriteLine("testdatasourcedirpath:" + testDatasourceDirPath);
                 Console.WriteLine("sourcedir: " + sourceDir);
+
+                //Create TestData folder in the deployment directory path
                 Directory.CreateDirectory(testDeploymentDirPath);
-                string testDataDestDirPath = ".\\TestData";
-                DirectoryInfo destDir = new DirectoryInfo(testDataDestDirPath);
-                Console.WriteLine("destDir: " + destDir);
+
+                //string testDataDestDirPath = ".\\TestData";
+                //DirectoryInfo destDir = new DirectoryInfo(testDataDestDirPath);
+                //Console.WriteLine("destDir: " + destDir);
+
+                //Copy files from data source path to deployment path
                 foreach (FileInfo file in sourceDir.GetFiles("*.*", SearchOption.TopDirectoryOnly))
                 {
-                    //file.CopyTo(Path.Combine(destDir.FullName, file.Name), true);
                     file.CopyTo(Path.Combine(testDeploymentDirPath, file.Name), true);
                 }
-                //Directory.CreateDirectory(testDestDirPath);
-                //foreach (FileInfo file in sourceDir.GetFiles("*.*", SearchOption.TopDirectoryOnly))
-                //    file.CopyTo(Path.Combine(testDestDirPath, file.Name), true);
                 Console.WriteLine("testdeplymentdirpath: " + testDeploymentDirPath);
             }
             catch (DirectoryNotFoundException ex)
@@ -143,19 +82,6 @@ namespace DsaTest.TestCases
 
         }
 
-        //// Use ClassInitialize to run code before running the first test in the class
-        /// <summary>
-        /// Tests the class initialize.
-        /// </summary>
-        /// <param name="testContext">The test context.</param>
-        [ClassInitialize()]
-        public static void TestClassInitialize(TestContext testContext)
-        {
-            //BaseClassInitialize(testContext); //Do not remove this line
-        }
-
-
-        //
         // Use TestInitialize to run code before running each test 
         /// <summary>
         /// Tests the initialize.
@@ -166,8 +92,7 @@ namespace DsaTest.TestCases
             try
             {
                 string navigateUrl = TestContext.Properties["BaseUrl"].ToString();
-                string userName = ConfigurationManager.AppSettings["UserName"];
-                TestWebDriver.NavigateHomePage(navigateUrl, userName);
+                TestWebDriver.NavigateHomePage(navigateUrl);
                 TestWebDriver.VerifyBusyOverlayNotDisplayed();
             }
             catch (Exception e)
@@ -192,17 +117,8 @@ namespace DsaTest.TestCases
                     TestWebDriver.TakeScreenshot(TestContext);
                 }
 
-                if (bool.Parse(ConfigurationManager.AppSettings["RunInSauceLabs"]) && ConfigurationManager.AppSettings["WebDriver"] == "RemoteWebDriver")
-                {
-                    IJavaScriptExecutor jse = (IJavaScriptExecutor)TestWebDriver;
-                    jse.ExecuteScript("sauce:job-result=" +
-                                      (TestContext.CurrentTestOutcome == UnitTestOutcome.Passed
-                                          ? "passed"
-                                          : "failed"));
-                }
-
                 TestWebDriver.Close();
-                AppDomain.CurrentDomain.ProcessExit += (s, e) => TestWebDriver.Close();
+                //AppDomain.CurrentDomain.ProcessExit += (s, e) => TestWebDriver.Close();
             }
             catch (Exception e)
             {
@@ -210,75 +126,6 @@ namespace DsaTest.TestCases
                 TestWebDriver.Dispose();
                 throw;
             }
-        }
-
-        /// <summary>
-        /// Resolves the BrowserOptions for SauceLabs based on the browser selected in App.config file.
-        /// </summary>
-        protected dynamic ResolveBrowserDriverOptions(string browserName)
-        {
-            var sauceOptions = new Dictionary<string, object>();
-            dynamic browserOptions;
-
-            switch (browserName.ToLower())
-            {
-                case "chrome":
-                    browserOptions = new ChromeOptions();
-                    browserOptions.UseSpecCompliantProtocol = true;
-                    browserOptions.PlatformName = ConfigurationManager.AppSettings["GridPlatform"];
-                    browserOptions.BrowserVersion = "latest";
-                    break;
-
-                case "legacyedge":
-                    browserOptions = new OpenQA.Selenium.Edge.EdgeOptions(); // legacy Edge
-                    browserOptions.PlatformName = ConfigurationManager.AppSettings["GridPlatform"];
-                    browserOptions.BrowserVersion = "latest";
-                    break;
-
-                case "edge":
-                    browserOptions = new Microsoft.Edge.SeleniumTools.EdgeOptions(); // edge chromium
-                    browserOptions.UseChromium = true;
-                    browserOptions.PlatformName = ConfigurationManager.AppSettings["GridPlatform"];
-                    browserOptions.BrowserVersion = "latest";
-                    break;
-
-                case "firefox":
-                    browserOptions = new FirefoxOptions();
-                    browserOptions.PlatformName = ConfigurationManager.AppSettings["GridPlatform"];
-                    browserOptions.BrowserVersion = "latest";
-                    break;
-
-                case "internetexplorer":
-                    browserOptions = new InternetExplorerOptions();
-                    browserOptions.PlatformName = ConfigurationManager.AppSettings["GridPlatform"];
-                    browserOptions.BrowserVersion = "latest";
-                    break;
-
-                case "safari":
-                    browserOptions = new SafariOptions();
-                    browserOptions.PlatformName = ConfigurationManager.AppSettings["GridPlatform"];
-                    browserOptions.BrowserVersion = "latest";
-                    break;
-
-                default:
-                    browserOptions = new ChromeOptions();
-                    browserOptions.UseSpecCompliantProtocol = true;
-                    browserOptions.PlatformName = ConfigurationManager.AppSettings["GridPlatform"];
-                    browserOptions.BrowserVersion = "latest";
-                    break;
-            }
-
-            sauceOptions.Add("username", ConfigurationManager.AppSettings["SauceLabsUserName"]);
-            sauceOptions.Add("accessKey", ConfigurationManager.AppSettings["SauceLabsAccessKey"]);
-            sauceOptions.Add("tunnelIdentifier", TestContext.Properties["SauceLabsTunnelIdentifier"].ToString());
-            sauceOptions.Add("parentTunnel", ConfigurationManager.AppSettings["SauceLabsParentTunnelName"]);
-            sauceOptions.Add("name", TestContext.TestName);
-            sauceOptions.Add("screenResolution", "1920x1080");
-
-            browserOptions.AddAdditionalCapability("sauce:options", sauceOptions, true);
-            
-            return browserOptions;
-
         }
 
         #endregion
